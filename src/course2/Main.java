0712e8c1c952833d6ab1db407527b0b8ad507ce6
Arthur Skotar <artur.skotar@gmail.com>
2016-03-29 12:44:50 +0300

    new branch

---
+++ src/course2/Main.java
@@ -0,0 +1,168 @@
+package course2;
+
+import java.io.*;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String path="C:\\Users\\Asus\\workspace\\course2\\src\\files\\input\\test.txt";
+        List<String>fileList=new LinkedList<>();
+        
+        try {
+			Files.walk(Paths.get("C:\\Users\\Asus\\workspace\\course2\\src\\files\\input")).forEach(filePath -> {
+			    if (Files.isRegularFile(filePath)) {
+			        
+			        fileList.add(filePath.toString());
+			    }
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println(fileList);
+        try {
+            fileReader(fileList,"UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       System.out.println(matcher("00B4"));
+    }
+    public static String fileReader(List paths, String encoding) throws IOException {
+        
+    	String result = null;
+        BufferedReader bufferedReader;
+        Map<String,Map<Character, Integer>> rezF=new TreeMap<>();
+        Map<Character,Integer> symb= new TreeMap<>();
+        File file=new File("C:\\Users\\Asus\\workspace\\course2\\src\\files\\output\\out.txt");
+      for(int j=0; j<paths.size();j++){
+    	  
+      
+    	  bufferedReader=new BufferedReader(new InputStreamReader(new FileInputStream(paths.get(j).toString()),encoding));
+        while ((result=bufferedReader.readLine())!=null) {
+                for (int i=0; i<result.length();i++){
+                    if (symb.containsKey(result.charAt(i))){
+                        int newVal=symb.get(result.charAt(i))+1;
+                        symb.replace(result.charAt(i), newVal);
+                    }else {
+                        symb.put(result.charAt(i),1);
+                    }
+                }
+
+
+        }
+        Path p=Paths.get(paths.get(j).toString());
+        rezF.put(p.getFileName().toString(), symb);
+        symb=new TreeMap<>();
+    }
+       
+
+            fileWriter(file,rezF);
+
+        
+    return result;
+}
+    public static void fileWriter(File file,Map<String,Map<Character, Integer>> rezF) throws IOException {
+    	Map<String, String>blankS=new TreeMap<>();
+    	
+    	Map <Character,Integer> symb=new TreeMap<>();
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        ArrayList<String> keys=new ArrayList<>(rezF.keySet());
+        Map<Character,Integer>allSymb=new TreeMap<>();
+        for(String i : keys){
+        	symb=rezF.get(i);
+        	
+        for (Map.Entry<Character, Integer> entry : symb.entrySet()) {
+            Character key = entry.getKey();
+            int val = entry.getValue();
+            int code = key;
+            if(allSymb.get(key)==null){
+            	allSymb.put(key, val);
+            }else{
+            	int newVal=val+allSymb.get(key);
+            	allSymb.replace(key, newVal);
+            }
+            String unicode = String.format("%04x", code);
+            String rez;
+            String desc="";
+            if(Symb.blankSymb.get(unicode) != null){
+            	String l=Symb.blankSymb.get(unicode);  
+            	desc=matcher(unicode);
+            	 rez = String.format("%-8s %8s %8s %8s %-15s %n",i, l, val, unicode,desc);
+            	 bw.write(rez+"\n");
+            }else{
+            	desc=matcher(unicode);
+             rez = String.format("%-8s %8s %8s %8s %-15s %n",i, key, val, unicode,desc);
+            
+            bw.write(rez+"\n");
+
+
+        }
+        }
+    }
+        bw.write("=======================================================\n");
+        for (Map.Entry<Character, Integer> ent : allSymb.entrySet()) {
+        	Character allKey = ent.getKey();
+            int allVal = ent.getValue();
+            int allCode = allKey;
+            String unicode = String.format("%04x", allCode);
+            String rez;
+            String desc="";
+            if(Symb.blankSymb.get(unicode) != null){
+            	String l=Symb.blankSymb.get(unicode); 
+            	desc=matcher(unicode);
+            	 rez = String.format("%-8s %8s %15s %-25s  %n", l, allVal, unicode,desc);
+            	 bw.write(rez+"\n");
+            }else{
+            	desc=matcher(unicode);
+             rez = String.format("%-8s %8s %15s %-25s %n", allKey, allVal ,unicode,desc);
+            
+            bw.write(rez+"\n");
+
+
+        }
+        }
+        System.out.println(allSymb);
+        bw.close();
+    }
+    
+    public static String matcher(String unicode) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Asus\\workspace\\course2\\src\\files\\add\\desc.txt"),"UTF-8"));
+		String line=null;
+		StringBuilder sb=null;
+		while((line=br.readLine())!=null){
+			if(line.contains(unicode.toUpperCase())){
+				String []ar=line.split(" ");
+				sb=new StringBuilder();
+				for(int i=4;i<ar.length;i++){
+					sb.append(ar[i]+" ");
+					
+				}
+			}
+		} br.close();
+		String rez;
+		try{
+		rez=""+sb.toString();
+		}catch(Exception e){
+			rez="";
+		}
+    	return rez;
+    	
+    }
+ 
+}
---
+++ src/course2/Symb.java
@@ -0,0 +1,89 @@
+package course2;
+
+import java.util.TreeMap;
+
+public class Symb {
+public static TreeMap<String, String> blankSymb=new TreeMap<>();
+static{
+	blankSymb.put("0000", "NUL");
+	blankSymb.put("0001", "SOH");
+	blankSymb.put("0002", "STX");
+	blankSymb.put("0003", "ETX");
+	blankSymb.put("0004", "EOT");
+	blankSymb.put("0005", "ENQ");
+	blankSymb.put("0006", "ACK");
+	blankSymb.put("0007", "BEL");
+	blankSymb.put("0008", "BS");
+	blankSymb.put("0009", "HT");
+	blankSymb.put("000A", "LF");
+	blankSymb.put("000B", "VT");
+	blankSymb.put("000C", "FF");
+	blankSymb.put("000D", "CR");
+	blankSymb.put("000E", "SO");
+	blankSymb.put("000F", "SI");
+	blankSymb.put("0010", "DLE");
+	blankSymb.put("0011", "DC1");
+	blankSymb.put("0012", "DC2");
+	
+	blankSymb.put("0013", "DC3");
+	blankSymb.put("0014", "DC4");
+	blankSymb.put("0015", "NAK");
+	blankSymb.put("0016", "SYN");
+	blankSymb.put("0017", "ETB");
+	blankSymb.put("0018", "CAN");
+	blankSymb.put("0019", "EM");
+	
+	blankSymb.put("001A", "SUB");
+	blankSymb.put("001B", "ESC");
+	blankSymb.put("001C", "FS");
+	blankSymb.put("001D", "GS");
+	blankSymb.put("001E", "RS");
+	blankSymb.put("001F", "US");
+	
+	blankSymb.put("0020", "SP");
+	blankSymb.put("0080", "XXX");
+	blankSymb.put("0081", "XXX");
+	blankSymb.put("0082", "BPH");
+	blankSymb.put("0083", "NBH");
+	blankSymb.put("0084", "IND");
+	
+	blankSymb.put("0085", "NEL");
+	blankSymb.put("0086", "SSA");
+	blankSymb.put("0087", "ESA");
+	blankSymb.put("0088", "HTS");
+	blankSymb.put("0089", "HTJ");
+	blankSymb.put("008A", "VTS");
+	
+	blankSymb.put("008B", "PLD");
+	blankSymb.put("008C", "PLU");
+	blankSymb.put("008D", "RI");
+	blankSymb.put("008E", "SS2");
+	blankSymb.put("008F", "SS3");
+	blankSymb.put("0090", "DCS");
+	
+	blankSymb.put("0091", "PU1");
+	blankSymb.put("0092", "PU2");
+	blankSymb.put("0093", "STS");
+	blankSymb.put("0094", "CCH");
+	blankSymb.put("0095", "MW");
+	blankSymb.put("0096", "SPA");
+	
+	blankSymb.put("0097", "EPA");
+	blankSymb.put("0098", "SOS");
+	blankSymb.put("0099", "XXX");
+	
+	blankSymb.put("009A-", "SCI");
+	blankSymb.put("009B", "CSI");
+	blankSymb.put("009C", "ST");
+	blankSymb.put("009D", "OSC");
+	blankSymb.put("009E", "APC");
+	blankSymb.put("009F", "SPA");
+	blankSymb.put("00A0", "NB SP");
+	blankSymb.put("feff", "ZWN BSP");
+	
+	
+	
+	
+	
+}
+}
